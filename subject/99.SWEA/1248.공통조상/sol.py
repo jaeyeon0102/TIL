9eae292b48b7d339c486d1abe55bc462dd101dd7
이재연_노트북 <wodus7792@gmail.com>
2025-08-04 23:14:45 +0900

    트리/그래프_과제완료

--- subject/99.SWEA/1248.공통조상/sol.py
+++ subject/99.SWEA/1248.공통조상/sol.py
@@ -1,8 +1,23 @@
-# import sys
+import sys
 
-# sys.stdin = open('input.txt')
+sys.stdin = open('input.txt')
 
 T = int(input())
+
+def get_ancestors(node):
+    path = [node]
+    while node in parent:
+        node = parent[node]
+        path.append(node)
+
+    return path
+
+def subtree(node):
+    size = 1
+    for child in children.get(node, []):
+        size += subtree(child)
+    return size
+
 
 for test_case in range(1, T+1):
     V, E, node1, node2 = map(int,input().split())
@@ -16,19 +31,15 @@ for test_case in range(1, T+1):
 
     for i in range(0, len(edge), 2):
         p,c = edge[i] , edge[i+1]
-
-        children.setdefault(p, []).append(c)
+        children.setdefault(p, []).append(c)        
         parent[c] = p
     
-    # print(parent)
+    result1 = get_ancestors(node1)
+    result2 = get_ancestors(node2)
 
-    result1 = []
+    for anc in result1:
+        if anc in result2:
+            common = anc
+            break
 
-    idx = node1
-    while (idx != None):
-        if parent[idx]:
-            result1.append(parent[idx]) 
-            idx = parent[idx]
-        else:
-            idx = None
-
+    print(f"#{test_case} {common} {subtree(common)}")
