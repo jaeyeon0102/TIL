64be6a23c08506f394ed58d0c339db3fc0739791
Lee jaeyeon <wodus7792@naver.com>
2025-09-05 17:37:52 +0900

    관통프로젝트_03.pjt_0905

---
+++ lec_project/03-pjt/examples/vue-project/src/components/ArticleList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Article List</h3>
+    <ArticleListItem 
+      v-for="article in articleStore.articles"
+      :key="article.id"
+      :article="article"
+    />
+  </div>
+</template>
+
+<script setup>
+import ArticleListItem from '@/components/ArticleListItem.vue'
+import { useArticleStore } from '@/stores/article';
+
+const articleStore = useArticleStore()
+</script>
---
+++ lec_project/03-pjt/examples/vue-project/src/views/ArticleView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Article Page</h1>
+    <ArticleList />
+  </div>
+</template>
+
+<script setup>
+import ArticleList from '@/components/ArticleList.vue';
+import { useArticleStore } from '@/stores/article';
+import { onMounted } from 'vue';
+
+const articleStore = useArticleStore()
+
+onMounted(() => {
+  articleStore.getArticles()
+})
+
+</script>
+
+<style>
+
+</style>
